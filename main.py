bea4c9562a7d1c79184cfef55a996bde7eec4b1d
wang sen <wangsen9509@gmail.com>
2026-03-10 01:35:06 +0800

    test

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 # This is a sample Python script.
+
+num1 = 1
+num2 = 1.0
+str1 = 'wangsen'
+str2 = 'python'
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Hello Python")
+    print(num1)
+    print(num2 / num1)
+    print(str1 + str2)
+    print(str2[-1:])
